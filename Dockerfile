a44f2d2d8cf78c0db1cac712f6c6118e7424df5a
Jamie Mason <jamie@foldleft.io>
2017-05-05 21:05:16 +0100

    Switch from sync-exec to async/await

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ COPY . /benchmarks
 ADD https://www.npmjs.com/install.sh ./install.sh
 RUN sh install.sh
 RUN npm install -g npm5@5.0.0-beta.36 shrinkpack@0.18.1 yarn@0.24.1
-RUN cd /benchmarks && npm install && npm cache clear
+WORKDIR /benchmarks
+RUN npm install && npm cache clear && npm run bundle
--- index.js
+++ index.js
@@ -1,91 +1,83 @@
 #!/usr/bin/env node
 
 const chalk = require('chalk');
-const exec = require('sync-exec');
 const path = require('path');
+const childProcess = require('child_process');
 
-const info = message => console.log(chalk.green(message))
-const warning = message => console.log(chalk.yellow(message))
-
-const directoriesByToolName = {
-  'npm': path.resolve('./npm'),
-  'npm-cached': path.resolve('./npm-cached'),
-  'shrinkpack': path.resolve('./shrinkpack'),
-  'shrinkpack-compressed': path.resolve('./shrinkpack-compressed'),
-  'yarn': path.resolve('./yarn'),
-  'npm5': path.resolve('./npm'),
-  'npm5-cached': path.resolve('./npm-cached'),
-  'npm5-shrinkpack': path.resolve('./shrinkpack'),
-  'npm5-shrinkpack-compressed': path.resolve('./shrinkpack-compressed'),
+const results = {
+  'npm/npm': { average: 0, runs: [] },
+  'npm/npm-cached': { average: 0, runs: [] },
+  'npm/shrinkpack': { average: 0, runs: [] },
+  'npm/shrinkpack-compressed': { average: 0, runs: [] },
+  'yarn/yarn': { average: 0, runs: [] },
+  'npm5/npm': { average: 0, runs: [] },
+  'npm5/npm-cached': { average: 0, runs: [] },
+  'npm5/shrinkpack': { average: 0, runs: [] },
+  'npm5/shrinkpack-compressed': { average: 0, runs: [] }
 };
 
-const runnersByToolName = {
-  'npm': () => exec('npm install', {
-    cwd: directoriesByToolName['npm'],
-    stdio: [2]
-  }),
-  'npm-cached': () => exec('npm install', {
-    cwd: directoriesByToolName['npm-cached'],
-    stdio: [2]
-  }),
-  'shrinkpack': () => exec('npm install', {
-    cwd: directoriesByToolName['shrinkpack'],
-    stdio: [2]
-  }),
-  'shrinkpack-compressed': () => exec('npm install', {
-    cwd: directoriesByToolName['shrinkpack-compressed'],
-    stdio: [2]
-  }),
-  'yarn': () => exec(`yarn install`, {
-    cwd: directoriesByToolName['yarn'],
-    stdio: [2]
-  }),
-  'npm5': () => exec('npm5 install', {
-    cwd: directoriesByToolName['npm5'],
-    stdio: [2]
-  }),
-  'npm5-cached': () => exec('npm5 install', {
-    cwd: directoriesByToolName['npm5-cached'],
-    stdio: [2]
-  }),
-  'npm5-shrinkpack': () => exec('npm5 install', {
-    cwd: directoriesByToolName['npm5-shrinkpack'],
-    stdio: [2]
-  }),
-  'npm5-shrinkpack-compressed': () => exec('npm5 install', {
-    cwd: directoriesByToolName['npm5-shrinkpack-compressed'],
-    stdio: [2]
-  }),
-};
-
-const runBenchmark = toolName => {
-  const directory = directoriesByToolName[toolName]
-  const run = runnersByToolName[toolName];
-  const cacheDir = path.join(directory, 'node_cache');
-  const installDir = path.join(directory, 'node_modules');
-
-  const start = new Date().getTime()
-  run();
-  const end = new Date().getTime();
-
-  const timeInSecs = (end - start) / 1000;
-  info(`${toolName}: ${timeInSecs}s`);
-
-  if (toolName.search(/^(npm-cached|npm5-cached)$/) !== -1) {
-    exec(`rm -rf ${cacheDir}`);
-  }
-
-  exec(`rm -rf ${installDir}`);
+function average (list) {
+  return list.reduce((total, value) => total + value, 0) / list.length;
 }
 
-runBenchmark('npm');
-runBenchmark('npm-cached');
-runBenchmark('shrinkpack');
-runBenchmark('shrinkpack-compressed');
+function spawn(cmd, args, options = {}) {
+  return new Promise((resolve, reject) => {
+    const proc = childProcess.spawn(cmd, args, options);
+    // proc.stdout.setEncoding('utf8');
+    // proc.stdout.on('data', data => process.stdout.write(chalk.grey(data.toString())));
+    // proc.stderr.setEncoding('utf8');
+    // proc.stderr.on('data', data => process.stdout.write(chalk.grey(data.toString())));
+    proc.on('close', code => code !== 0 ? reject() : resolve());
+  });
+}
 
-runBenchmark('yarn');
+async function clean(dirPath) {
+  await spawn('npm', ['cache', 'clean']);
+  await spawn('yarn', ['cache', 'clean']);
+  if (dirPath.includes('-cached') == false) {
+    await spawn('rm', ['-rf', path.join(dirPath, 'node_cache')]);
+  }
+  await spawn('rm', ['-rf', path.join(dirPath, 'node_modules')]);
+}
 
-runBenchmark('npm5');
-runBenchmark('npm5-cached');
-runBenchmark('npm5-shrinkpack');
-runBenchmark('npm5-shrinkpack-compressed');
+async function runBenchmark(installer, directory) {
+  const key = `${installer}/${directory}`;
+  const dirPath = path.resolve(directory);
+
+  await clean(dirPath);
+  const start = new Date().getTime();
+  await spawn(installer, ['install'], { cwd: dirPath });
+  const end = new Date().getTime();
+  await clean(dirPath);
+
+  const time = (end - start) / 1000;
+  const result = results[key];
+  result.runs.push(time);
+  result.average = average(result.runs);
+  console.log(`${key}: ${time}s (average ${result.average}s over ${result.runs.length} runs)`);
+  return time;
+}
+
+async function runAll() {
+  try {
+    const sampleSize = 5;
+    for (let i = 1; i <= sampleSize; i++) {
+      console.log(chalk.underline(`Run ${i}`));
+      await runBenchmark('npm', 'npm');
+      await runBenchmark('npm', 'npm-cached');
+      await runBenchmark('npm', 'shrinkpack');
+      await runBenchmark('npm', 'shrinkpack-compressed');
+      await runBenchmark('yarn', 'yarn');
+      await runBenchmark('npm5', 'npm');
+      await runBenchmark('npm5', 'npm-cached');
+      await runBenchmark('npm5', 'shrinkpack');
+      await runBenchmark('npm5', 'shrinkpack-compressed');
+      console.log('');
+    }
+  } catch (err) {
+    console.error(chalk.red(err));
+    process.exit(1);
+  }
+}
+
+runAll();
