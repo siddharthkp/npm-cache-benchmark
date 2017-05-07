07f12291b65c049a57ecd6914a9ab0c20609683e
Jamie Mason <jamie@foldleft.io>
2017-05-07 09:22:06 +0100

    Add a script to verify each installation

--- index.js
+++ index.js
@@ -2,92 +2,25 @@ const path = require('path');
 const childProcess = require('child_process');
 
 const results = {
-  'npm/npm': {
-    average: 0,
-    name: 'npm 4.x',
-    runs: []
-  },
-  'npm/npm-cached': {
-    average: 0,
-    name: 'npm 4.x (cached)',
-    runs: []
-  },
-  'npm/shrinkpack': {
-    average: 0,
-    name: 'npm 4.x (shrinkpacked)',
-    runs: []
-  },
-  'npm/shrinkpack-compressed': {
-    average: 0,
-    name: 'npm 4.x (shrinkpacked, compressed)',
-    runs: []
-  },
-  'yarn/yarn': {
-    average: 0,
-    name: 'yarn',
-    runs: []
-  },
-  'yarn/yarn-offline': {
-    average: 0,
-    name: 'yarn --offline',
-    runs: []
-  },
-  'npm5/npm': {
-    average: 0,
-    name: 'npm 5.x',
-    runs: []
-  },
-  'npm5/npm-cached': {
-    average: 0,
-    name: 'npm 5.x (cached)',
-    runs: []
-  },
-  'npm5/shrinkpack': {
-    average: 0,
-    name: 'npm 5.x (shrinkpacked)',
-    runs: []
-  },
-  'npm5/shrinkpack-compressed': {
-    average: 0,
-    name: 'npm 5.x (shrinkpacked, compressed)',
-    runs: []
-  }
+  'npm/npm': { average: 0, name: 'npm 4.x', runs: [] },
+  'npm/npm-cached': { average: 0, name: 'npm 4.x (cached)', runs: [] },
+  'npm/shrinkpack': { average: 0, name: 'npm 4.x (shrinkpacked)', runs: [] },
+  'npm/shrinkpack-compressed': { average: 0, name: 'npm 4.x (shrinkpacked, compressed)', runs: [] },
+  'yarn/yarn': { average: 0, name: 'yarn', runs: [] },
+  'yarn/yarn-offline': { average: 0, name: 'yarn --offline', runs: [] },
+  'npm5/npm': { average: 0, name: 'npm 5.x', runs: [] },
+  'npm5/npm-cached': { average: 0, name: 'npm 5.x (cached)', runs: [] },
+  'npm5/shrinkpack': { average: 0, name: 'npm 5.x (shrinkpacked)', runs: [] },
+  'npm5/shrinkpack-compressed': { average: 0, name: 'npm 5.x (shrinkpacked, compressed)', runs: [] }
 };
 
-function average(list) {
-  return list.reduce((total, value) => total + value, 0) / list.length;
+try {
+  runAll();
+} catch (err) {
+  onError(err);
 }
 
-function spawn(cmd, args, options = {}) {
-  childProcess.spawnSync(cmd, args, options);
-}
-
-function clean(dirPath) {
-  spawn('npm', ['cache', 'clean']);
-  spawn('yarn', ['cache', 'clean']);
-  if (dirPath.includes('-cached') === false) {
-    spawn('rm', ['-rf', path.join(dirPath, 'node_cache')]);
-  }
-  spawn('rm', ['-rf', path.join(dirPath, 'node_modules')]);
-}
-
-function runBenchmark(installer, args, directory) {
-  const key = `${installer}/${directory}`;
-  const dirPath = path.resolve(directory);
-
-  clean(dirPath);
-  const start = new Date().getTime();
-  spawn(installer, args, { cwd: dirPath });
-  const end = new Date().getTime();
-  clean(dirPath);
-
-  const time = (end - start) / 1000;
-  const result = results[key];
-  result.runs.push(time);
-  result.average = average(result.runs);
-
-  console.log(`${result.name}: ${time.toFixed(2)}s (average ${result.average.toFixed(2)}s)`);
-}
+// implementation
 
 function runAll() {
   const sampleSize = 5;
@@ -107,9 +40,54 @@ function runAll() {
   }
 }
 
-try {
-  runAll();
-} catch (err) {
+function runBenchmark(installer, args, directory) {
+  const key = `${installer}/${directory}`;
+  const dirPath = path.resolve(directory);
+
+  clean(dirPath);
+  const start = new Date().getTime();
+  spawn(installer, args, dirPath);
+  const end = new Date().getTime();
+  verify(dirPath);
+  clean(dirPath);
+
+  const time = (end - start) / 1000;
+  const result = results[key];
+  result.runs.push(time);
+  result.average = average(result.runs);
+
+  console.log(`${result.name}: ${time.toFixed(2)}s (average ${result.average.toFixed(2)}s)`);
+}
+
+function spawn(cmd, args, cwd) {
+  const shell = childProcess.spawnSync(cmd, args, {
+    cwd: cwd || process.cwd(),
+    encoding: 'utf-8',
+    stdio: 'pipe'
+  });
+  const stdout = shell.output[1];
+  const stderr = shell.output[2];
+  shell.status !== 0 && onError(stderr);
+}
+
+function clean(dirPath) {
+  spawn('npm', ['cache', 'clean'], dirPath);
+  spawn('yarn', ['cache', 'clean'], dirPath);
+  if (dirPath.includes('-cached') === false) {
+    spawn('rm', ['-rf', path.join(dirPath, 'node_cache')]);
+  }
+  spawn('rm', ['-rf', path.join(dirPath, 'node_modules')]);
+}
+
+function verify(dirPath) {
+  spawn('node', ['./verify.js'], dirPath);
+}
+
+function average(list) {
+  return list.reduce((total, value) => total + value, 0) / list.length;
+}
+
+function onError(err) {
   console.error(err);
   process.exit(1);
 }
